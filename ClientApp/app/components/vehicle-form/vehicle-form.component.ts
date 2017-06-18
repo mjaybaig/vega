7c5cbfd70c889eadc2c447b3085261605edc111b
M. Jameel Baig <mjameelbaig@hotmail.com>
2017-06-19 00:40:19 +0500

    implemented crud

---
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,27 @@
+import { VehicleService } from './../../services/vehicle.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes:any[];
+  vehicle:any = {};
+  models:any[];
+  features:any[];
+
+  constructor(private vehicleService: VehicleService) { }
+
+  ngOnInit() {
+    this.vehicleService.getMakes().subscribe( makes => this.makes = makes );
+    this.vehicleService.getFeatures().subscribe( feats => this.features = feats);
+  }
+
+  onMakeChange(){
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make)
+    this.models = selectedMake ? selectedMake.models : [];
+  }
+
+}
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using vega.Controllers.Resources;
 using vega.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace vega.Mapping
 {
@@ -8,9 +10,45 @@ namespace vega.Mapping
     {
         public MappingProfile()
         {
+            //Domain to API Resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone}))
+                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
+
+            //API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.Id, opt => opt.Ignore())
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vf => vf.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vf => vf.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vf => vf.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.Ignore())
+                .AfterMap((vr, v) => {
+                   //remove unselected features
+                //    var removedFeatures = new List<VehicleFeature>();
+                //    foreach(var f in v.Features){
+                //        if(!vr.Features.Contains(f.FeatureId)){
+                //            removedFeatures.Add(f);
+                //        }
+                //    }
+                   var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
+                   foreach(var r in removedFeatures.ToList()){
+                       v.Features.Remove(r);
+                   }
+
+                   //add new features
+                //    foreach(var id in vr.Features){
+                //        if(!v.Features.Any(f => f.FeatureId == id)){
+                //            v.Features.Add(new VehicleFeature{FeatureId = id});
+                //        }
+                //    }
+                   var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature{ FeatureId = id });
+                   foreach(var f in addedFeatures.ToList()){
+                       v.Features.Add(f);
+                   }
+                });
         }
     }
 }
